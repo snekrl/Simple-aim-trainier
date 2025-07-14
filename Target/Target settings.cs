67450af752e464df220e5cedd4dca23f72b77ade
snek <snekom28@gmail.com>
2025-07-15 01:55:26 +0200

    attempt at respawning the ball
    BUGGED DO NOT RUN

--- Target/Target settings.cs
+++ Target/Target settings.cs
@@ -3,11 +3,29 @@ using UnityEngine;
 
 public class Targetsettings : MonoBehaviour
 {
+
+
+    private float timePassed = 0.0f;
+    bool hit = false;
+
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "bullet")
         {
+            hit = true;
             Destroy(gameObject);
         }
+    }
+
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+
+        if (hit = true)
+        {
+            Instantiate(gameObject);
+        }
+
     }
 }
