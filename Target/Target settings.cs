16ef07c42bcc5df40dfc119bf237d770723e70ec
cyrox <aidena189@gmail.com>
2025-07-15 12:54:47 -0400

    Update Target settings.cs

--- Target/Target settings.cs
+++ Target/Target settings.cs
@@ -1,31 +1,34 @@
-using System;
 using UnityEngine;
 
 public class Targetsettings : MonoBehaviour
 {
-
-
     private float timePassed = 0.0f;
     bool hit = false;
 
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "bullet")
+        if (collision.gameObject.CompareTag("bullet"))
         {
             hit = true;
-            Destroy(gameObject);
+            gameObject.transform.localScale = new Vector3(0,0,0);
+            timePassed = 0.0f;
         }
     }
 
-    void Update()
+    private void Update()
     {
         timePassed += Time.deltaTime;
 
-        if (hit = true)
+        if(hit && timePassed >= 0.5f)
         {
-            Instantiate(gameObject);
+            var minPosition = new Vector3(-45, 0, 20);
+            var maxPosition = new Vector3(-35, 10, 20);
+            var randomPosition = new Vector3(Random.Range(minPosition.x, maxPosition.x), Random.Range(minPosition.y, maxPosition.y), Random.Range(minPosition.z, maxPosition.z));
+            gameObject.transform.localPosition = (randomPosition);
+            gameObject.transform.localScale = new Vector3(1,1,1);
+            hit = false;
+            timePassed = 0.0f;
         }
-
     }
 }
