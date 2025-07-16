bba3d6c5796f0bb8ebb6b8bde18438af0d98f2c7
snek <snekom28@gmail.com>
2025-07-17 00:01:14 +0200

    messing around

--- Dummy/Dummy AI.cs
+++ Dummy/Dummy AI.cs
@@ -13,6 +13,10 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
-        
+        var minPosition = new Vector3(-45, 0, 20);
+        var maxPosition = new Vector3(-35, 10, 20);
+        var randomPosition = new Vector3(Random.Range(minPosition.x, maxPosition.x), Random.Range(minPosition.y, maxPosition.y), Random.Range(minPosition.z, maxPosition.z));
+        gameObject.transform.localPosition = (randomPosition);
+        gameObject.transform.localScale = new Vector3(1, 1, 1);
     }
 }
